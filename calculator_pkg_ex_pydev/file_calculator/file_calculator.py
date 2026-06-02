b5d4ca02091ea17a407f48c9d624ce9bbcda039d
Devon <ds.pydev@gmail.com>
2026-06-02 14:30:35 +0200

    file calculator implemented

--- calculator_pkg_ex_pydev/file_calculator/file_calculator.py
+++ calculator_pkg_ex_pydev/file_calculator/file_calculator.py
@@ -1,7 +1,26 @@
+from pathlib import Path
+
+# from tqdm import tqdm
+
 from ..calculator import Calculator  # noqa Relative path
 
 # from calculator_pkg_ex.calculator import Calculator #Absolute path
 
 class FileCalculator(Calculator):
-    def __init__(self):
-        pass
+	def __init__(self,
+		path: Path = Path(__file__).parent / "nums.csv",
+	):
+		self.path = path
+		
+	def add_file(self):
+		total = None
+		with open(self.path) as f:
+			for line in f:
+				if total is None:
+					total = line
+					continue
+				else:
+					total += line
+		
+		return total
+			
