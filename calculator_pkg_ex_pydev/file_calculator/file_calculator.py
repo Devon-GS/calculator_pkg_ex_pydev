43018c9d4d020eca78174ebfec1efa29c19cc1f3
Devon <ds.pydev@gmail.com>
2026-06-02 14:00:44 +0200

    updates

---
+++ calculator_pkg_ex_pydev/file_calculator/file_calculator.py
@@ -0,0 +1,7 @@
+from ..calculator import Calculator  # noqa Relative path
+
+# from calculator_pkg_ex.calculator import Calculator #Absolute path
+
+class FileCalculator(Calculator):
+    def __init__(self):
+        pass
